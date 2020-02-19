c10dd400ab44f63f5f60d36608372a2de7e775f7
jamiha kammeraad <jamihakammeraad25@gmail.com>
2020-02-19 09:54:10 -0500

    Made changes for testing purposes
    The majority of the changes I made were to test one side of the drivetrain but I also made some extra comments so people can understand the code a little better.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,8 +9,11 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.*;
 
 //Rev Imports
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.EncoderType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
 
 public class Robot extends TimedRobot {
   //Left Side Spark Max's
@@ -19,34 +22,56 @@ public class Robot extends TimedRobot {
   private CANSparkMax topLeft = new CANSparkMax(3, MotorType.kBrushless); 
 
   //Right Side Spark Max's
-  private CANSparkMax bottomRightBack = new CANSparkMax(4, MotorType.kBrushless);
+  /*private CANSparkMax bottomRightBack = new CANSparkMax(4, MotorType.kBrushless);
   private CANSparkMax bottomRightFront = new CANSparkMax(5, MotorType.kBrushless);
-  private CANSparkMax topRight = new CANSparkMax(6, MotorType.kBrushless);
+  private CANSparkMax topRight = new CANSparkMax(6, MotorType.kBrushless);*/
+
+//private CANSparkMax leftSpin = new CANSparkMax(8, MotorType.kBrushless);
+//private CANSparkMax rightSpin = new CANSparkMax(9, MotorType.kBrushless);
 
   //Victor Implementation
-  private VictorSP leftSnowblower = new VictorSP(0);
+  //private VictorSP leftSnowblower = new VictorSP(0);
   //private VictorSP rightSnowblower = new VictorSP(1);
 
   //Differential Drive for our Three Wheel NEO
-  private DifferentialDrive threeWheel = new DifferentialDrive(topLeft, topRight);
+  //private DifferentialDrive threeWheel = new DifferentialDrive(topLeft, topRight);
 
   //Xbox Controller
-  XboxController controller = new XboxController(1);
+  XboxController controller = new XboxController(0);
+
+  //double distance;
+
+  //double circumfrence;
+
+  //CANEncoder encoder;
+
+  private Timer time = new Timer();
+
+  //double CPR = encoder.getCountsPerRevolution();
+  //double pos = encoder.getPosition();
 
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
 
   public void autonomousInit() {
-    bottomLeftFront.follow(topLeft);
+    /*bottomLeftFront.follow(topLeft);
     bottomLeftBack.follow(topLeft);
 
     bottomRightFront.follow(topRight);
-    bottomRightBack.follow(topRight);
+    bottomRightBack.follow(topRight);*/
+
+    time.reset();
+
+    time.start();
   }
 
   public void autonomousPeriodic() {
-    
+    //circumfrence = 2 * Math.PI * 3;
+
+    //distance = (pos/CPR) * circumfrence;
+
+
   }
 
 
@@ -56,19 +81,43 @@ public class Robot extends TimedRobot {
     bottomLeftFront.follow(topLeft);
     bottomLeftBack.follow(topLeft);
 
-    bottomRightFront.follow(topRight);
-    bottomRightBack.follow(topRight);
+    //bottomRightFront.follow(topRight);
+    //bottomRightBack.follow(topRight);
+
+    //leftSpin.setInverted(true);
   }
 
   public void teleopPeriodic() {
-    if(controller.getRawAxis(5) > 0.7){
+    /*if(controller.getRawAxis(5) > 0.7){
       leftSnowblower.setSpeed(0.7);
       //rightSnowblower.setSpeed(-0.7);
     }else if(controller.getRawAxis(5)< -0.7){
       leftSnowblower.setSpeed(-0.7);
       //rightSnowblower.setSpeed(0.7);
+    }else {
+      leftSnowblower.set(0);
+    }*/
+
+    /*if(controller.getRawButton(5) == true){
+      leftSpin.set(0.5);
+      rightSpin.set(0.5);
+    }else if(controller.getRawButton(6) == true){
+      leftSpin.set(-0.5);
+      rightSpin.set(-0.5);
+    }else{
+      leftSpin.set(0);
+      rightSpin.set(0);
+    }*/
+
+    if(controller.getRawAxis(1) > 0.6){
+      topLeft.set(1);
+      
+    }else if(controller.getRawAxis(0) > .5){
+      topLeft.set(-1);
+    }else{
+      topLeft.set(0.0);
     }
 
-    threeWheel.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(0));
+    //threeWheel.arcadeDrive(controller.getRawAxis(1), controller.getRawAxis(0));
     }
   }
